cd7b6e8a392edd34d1dd78523924999d5ee0954d
sultangali <sultangali.zhumagaliyev@gmail.com>
2025-05-07 20:42:45 +0500

    sl

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:5173', // Vite's default port
+    origin: 'https://34.34.73.209', // Vite's default port
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -43,7 +43,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(cors({
-  origin: 'http://localhost:5173', // Vite's default port
+  origin: 'https://34.34.73.209', // Vite's default port
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control', 'Pragma', 'Content-Length'],
   exposedHeaders: ['Content-Length', 'Content-Type'],
